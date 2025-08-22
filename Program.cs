d3401719a50e1c18ebdb5330a11a25c3c0eeebd2
zapatovolador <caramaleon@hotmail.com>
2025-08-22 21:50:04 +0200

    Update comments Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
 using InventoryApp.Repositories;
-
+/*
+ * Configuring services for the application
+ */
 var builder = WebApplication.CreateBuilder(args);
 
+// Registering the in-memory repository as a singleton service
 builder.Services.AddSingleton<IProductRepository, InMemoryProductRepository>();
 builder.Services.AddControllers();
     
 builder.Services.AddEndpointsApiExplorer();
+// Adding Swagger for testing API
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
